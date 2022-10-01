10d671d73897f363be067e57d146696a384948ec
Ashwini <113336360+ashwini77088@users.noreply.github.com>
2022-10-01 09:18:39 +0530

    okey

---
+++ loops.js
@@ -0,0 +1,38 @@
+// for and while loop assignment-1
+//assign 1
+let a=1
+
+while(a<=10){
+    console.log(a)
+    a=a+1
+}
+
+
+//for and while loop assignment-2
+ let b=10
+
+while(b>=1){
+    console.log(b)
+    b=b-1
+}
+
+//for loop assignment-3
+//5,6,7,8,...15
+for (let i=5;i<=5;i=i+1){
+    console.log(i)
+}
+//for loop assignment-4
+
+for (let j=15;j>=10;j=j-1){
+    console.log(j)
+}
+//even number while 1 to 10 assignment 5
+let c=2
+while(c<=10){
+    console.log(c)
+    c=c+2
+}
+// odd number while 1 to 10 assignment 6
+ for (let k=1;k>=10;k=k-1){
+ console.log(k)
+ }
